9cb04df3d2ca3c6ea1e81617453e47c3df331748
Han <yulmam1@naver.com>
2023-11-10 16:58:09 +0900

    feat(InputView) : 사용자에게 메뉴를 입력 받는 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,13 +1,22 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.enumeration.Menu;
+
+import java.util.Map;
 
 import static christmas.util.Parser.stringToInt;
+import static christmas.util.Parser.stringToMap;
 
 public class InputView {
 
     public int requestVisitDay() throws IllegalArgumentException{
         System.out.println("12월 중 예상 방문 날짜는 언제인가요?");
         return stringToInt(Console.readLine());
+    }
+
+    public Map<Menu, Integer> requestOrder()throws IllegalArgumentException{
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        return stringToMap(Console.readLine());
     }
 }
